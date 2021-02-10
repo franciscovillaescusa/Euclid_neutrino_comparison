d890280c87eb4d789c0587c64cdc06487e412dcb
franciscovillaescusa <villaescusa.francisco@gmail.com>
2021-02-10 11:54:54 -0500

    update

--- Analysis/0.15eV/expected_Pk.py
+++ Analysis/0.15eV/expected_Pk.py
@@ -6,21 +6,41 @@ import sys,os
 BoxSize = 512.0 #Mpc/h
 grid    = 1024
 
-fin     = '../../param_files/0.15eV/reps_files/0.15eV_Pm_rescaled_z127.0000.txt'
-fout    = 'Pk_binned_CLASS_0.15eV_matter_z=127.txt'
+fins = ['../../param_files/0.15eV/reps_files/0.15eV_Pm_rescaled_z0.0000.txt',
+        '../../param_files/0.15eV/reps_files/0.15eV_Pcb_rescaled_z0.0000.txt',
+        '../../param_files/0.15eV/reps_files/0.15eV_Pn_rescaled_z0.0000.txt',
+        '../../param_files/0.15eV/reps_files/0.15eV_Pm_rescaled_z0.5000.txt',
+        '../../param_files/0.15eV/reps_files/0.15eV_Pcb_rescaled_z0.5000.txt',
+        '../../param_files/0.15eV/reps_files/0.15eV_Pn_rescaled_z0.5000.txt',
+        '../../param_files/0.15eV/reps_files/0.15eV_Pm_rescaled_z1.0000.txt',
+        '../../param_files/0.15eV/reps_files/0.15eV_Pcb_rescaled_z1.0000.txt',
+        '../../param_files/0.15eV/reps_files/0.15eV_Pn_rescaled_z1.0000.txt',
+        '../../param_files/0.15eV/reps_files/0.15eV_Pm_rescaled_z2.0000.txt',
+        '../../param_files/0.15eV/reps_files/0.15eV_Pcb_rescaled_z2.0000.txt',
+        '../../param_files/0.15eV/reps_files/0.15eV_Pn_rescaled_z2.0000.txt'] 
 
-#fin     = '../../param_files/0.15eV/reps_files/0.15eV_Pcb_rescaled_z127.0000.txt'
-#fout    = 'Pk_binned_CLASS_0.15eV_cb_z=127.txt'
-
-#fin     = '../../param_files/0.15eV/reps_files/0.15eV_Pn_rescaled_z127.0000.txt'
-#fout    = 'Pk_binned_CLASS_0.15eV_n_z=127.txt'
+fouts = ['Pk_binned_CLASS_0.15eV_matter_z=0.txt',
+         'Pk_binned_CLASS_0.15eV_cb_z=0.txt',
+         'Pk_binned_CLASS_0.15eV_n_z=0.txt',
+         'Pk_binned_CLASS_0.15eV_matter_z=0.5.txt',
+         'Pk_binned_CLASS_0.15eV_cb_z=0.5.txt',
+         'Pk_binned_CLASS_0.15eV_n_z=0.5.txt',
+         'Pk_binned_CLASS_0.15eV_matter_z=1.txt',
+         'Pk_binned_CLASS_0.15eV_cb_z=1.txt',
+         'Pk_binned_CLASS_0.15eV_n_z=1.txt',
+         'Pk_binned_CLASS_0.15eV_matter_z=2.txt',
+         'Pk_binned_CLASS_0.15eV_cb_z=2.txt',
+         'Pk_binned_CLASS_0.15eV_n_z=2.txt']
 ######################################################################################
 
-# read Pk
-k, Pk = np.loadtxt(fin, unpack=True)
-k  = k.astype(np.float32)
-Pk = Pk.astype(np.float32)
+# do a loop over the different files
+for fin,fout in zip(fins, fouts):
 
-# compute binned Pk and save results to file
-k, Pk, Nmodes = PKL.expected_Pk(k, Pk, BoxSize, grid)
-np.savetxt(fout, np.transpose([k, Pk]))
+    # read Pk
+    k, Pk = np.loadtxt(fin, unpack=True)
+    k  = k.astype(np.float32)
+    Pk = Pk.astype(np.float32)
+
+    # compute binned Pk and save results to file
+    k, Pk, Nmodes = PKL.expected_Pk(k, Pk, BoxSize, grid)
+    np.savetxt(fout, np.transpose([k, Pk]))
